86049965c0cebfe355677720f9109258a08a040d
Peter Bartosch <bartoschp@gmail.com>
2015-02-14 21:33:02 -0600

    Updates to animations.  I think.  I don't know what the fuck I'm doing.

--- Assets/Scripts/AI/SwarmerAI.cs
+++ Assets/Scripts/AI/SwarmerAI.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using OgreToast.Utility;
 using OgreToast.Attributes;
 
-[RequireComponent(typeof(FlyingComponent))]
+[RequireComponent(typeof(FlyingComponent), typeof(SpriteRenderer), typeof(HealthComponent))]
 public class SwarmerAI : BaseAI<SwarmerAI>
 {
 	#region patrol_public_vars
@@ -22,7 +22,7 @@ public class SwarmerAI : BaseAI<SwarmerAI>
 	public float MinScreenPct = 0.1f;
 	public float MaxScreenPct = 0.9f;
 	public float PatrolSpeed = 25f;
-	public float PauseAtNodeTime = 0.5f;
+	public float PauseTimeAtEdge = 0.1f;
 	#endregion
 	
 	#region sensing_public_vars
@@ -58,6 +58,40 @@ public class SwarmerAI : BaseAI<SwarmerAI>
 
 	private void Start()
 	{
+		Vector3 moveDelta = new Vector3(((IsMovingRight)?1:-1) * Random.Range(HorizontalDistRange.x, HorizontalDistRange.y),
+		                                ((IsMovingUp)?1:-1) * Random.Range(VerticalDistRange.x, VerticalDistRange.y),
+		                                0f);
+		Vector3 viewportPos = Camera.main.WorldToViewportPoint(transform.position + moveDelta);
+		viewportPos.x = Mathf.Min(Mathf.Max(viewportPos.x, MinScreenPct), MaxScreenPct);
+		viewportPos.y = Mathf.Min(Mathf.Max(viewportPos.y, MinScreenPct), MaxScreenPct);
+		_moveTarget = Camera.main.ViewportToWorldPoint(viewportPos);
+		_mover = GetComponent<FlyingComponent>();
+		_pauseTimer = new SimpleTimer(PauseTimeAtEdge);
+		_pauseTimer.Start();
+		if(DoPatrolAtStart)
+		{
+			currentState = PatrolAndAttackState;
+		}
+		
+		if(MaxObjectsToFind <= 0)
+		{
+			MaxObjectsToFind = 1;
+		}
+		_sensedColliders = new Collider2D[MaxObjectsToFind];
+		_realSensorCenter = (Vector2)transform.position + SensorCenter;
+		
+		_attackExpireTimer = new SimpleTimer(TimeBeforeAttackExpires);
+		_weapon = transform.FindChild("gun").gameObject.GetComponent<WeaponComponent>();
+		_stunTimer = new SimpleTimer(HitStunTime);
+		_health = GetComponent<HealthComponent>();
+		_health.Dead += OnDead;
+		_health.Hit += OnHit;
+		
+		if(Mathf.RoundToInt(Mathf.Cos(transform.rotation.eulerAngles.y * Mathf.Deg2Rad)) == -1)
+		{
+			SensorCenter.x *= -1;
+		}
+
 		rigidbody2D.gravityScale = 0f;
 	}
 
@@ -65,10 +99,10 @@ public class SwarmerAI : BaseAI<SwarmerAI>
 	{
 		if(ShowEndPointsInEditor)
 		{
-			float midPct = (MaxScreenPct - MinScreenPct) / 2f;
+			float midPct = MinScreenPct + (MaxScreenPct - MinScreenPct) / 2f;
 			Vector3 leftCenter = Camera.main.ViewportToWorldPoint(new Vector3(MinScreenPct, midPct, Camera.main.transform.position.z));
 			Vector3 rightCenter = Camera.main.ViewportToWorldPoint(new Vector3(MaxScreenPct, midPct, Camera.main.transform.position.z));
-			Vector3 size = new Vector3(collider2D.bounds.size.x, (MaxScreenPct - MinScreenPct) * Screen.height, 1f);
+			Vector3 size = new Vector3(collider2D.bounds.size.x, (MaxScreenPct - MinScreenPct) * Camera.main.pixelHeight, 1f);
 			Gizmos.color = new Color(1f, 0.5f, 0f, 0.5f);
 			SpriteRenderer sr = GetComponent<SpriteRenderer>();
 			Gizmos.DrawCube(leftCenter, size);
@@ -87,7 +121,7 @@ public class SwarmerAI : BaseAI<SwarmerAI>
 		}
 	}
 
-	protected virtual void PatrolState()
+	protected virtual void PatrolAndAttackState()
 	{
 		if(Mathf.Abs(transform.position.x - _moveTarget.x) < 1f && !_pauseTimer.IsRunning)
 		{
@@ -98,12 +132,12 @@ public class SwarmerAI : BaseAI<SwarmerAI>
 			Vector3 viewportPos;
 			if(IsMovingRight)
 			{
-				viewportPos = Camera.main.WorldToViewportPoint(transform.position + collider2D.bounds.extents.x * Vector3.right);
-				IsMovingRight = viewportPos.x >= MaxScreenPct;
+				viewportPos = Camera.main.WorldToViewportPoint(collider2D.bounds.max);
+				IsMovingRight = viewportPos.x < MaxScreenPct;
 			}
 			else
 			{
-				viewportPos = Camera.main.WorldToViewportPoint(transform.position - collider2D.bounds.extents.x * Vector3.right);
+				viewportPos = Camera.main.WorldToViewportPoint(collider2D.bounds.min);
 				IsMovingRight = viewportPos.x <= MinScreenPct;
 			}
 			Vector3 moveDelta = new Vector3(((IsMovingRight)?1:-1) * Random.Range(HorizontalDistRange.x, HorizontalDistRange.y),
@@ -112,6 +146,18 @@ public class SwarmerAI : BaseAI<SwarmerAI>
 			viewportPos = Camera.main.WorldToViewportPoint(transform.position + moveDelta);
 			viewportPos.x = Mathf.Min(Mathf.Max(viewportPos.x, MinScreenPct), MaxScreenPct);
 			viewportPos.y = Mathf.Min(Mathf.Max(viewportPos.y, MinScreenPct), MaxScreenPct);
+			if(viewportPos.y == MaxScreenPct && IsMovingUp)
+			{
+				float otherBound = (MaxScreenPct-MinScreenPct)/2f;
+				viewportPos.y = (otherBound < MinScreenPct) ? Random.Range(otherBound, MinScreenPct) : Random.Range(MinScreenPct, otherBound);
+				IsMovingUp = false;
+			}
+			else if(viewportPos.y == MinScreenPct && !IsMovingUp)
+			{
+				float otherBound = (MaxScreenPct-MinScreenPct)/2f;
+				viewportPos.y = (otherBound < MaxScreenPct) ? Random.Range(otherBound, MaxScreenPct) : Random.Range(MaxScreenPct, otherBound);
+				IsMovingUp = true;
+			}
 			_moveTarget = Camera.main.ViewportToWorldPoint(viewportPos);
 		}
 		else if(_pauseTimer.IsExpired)
@@ -121,6 +167,57 @@ public class SwarmerAI : BaseAI<SwarmerAI>
 			float xMod = (IsMovingRight) ? 1f : -1f;
 			float yMod = (IsMovingUp) ? 1f : -1f;
 			_mover.Fly(xMod, yMod);
+		}
+		//check for player and change state as needed.
+		if(HasSensedSomething() && CanAttack)
+		{
+			_attackExpireTimer.Stop();
+			//aim and then fire
+			Transform target = _sensedColliders[0].transform;
+			Vector3 targetPos = target.position;
+			if(target.rigidbody2D != null)
+			{
+				targetPos += (Vector3)target.rigidbody2D.velocity * Time.deltaTime;
+			}
+			
+			Vector2 dirToTarget = (targetPos - transform.position).normalized;
+			dirToTarget.x = Mathf.RoundToInt(dirToTarget.x);
+			dirToTarget.y = Mathf.RoundToInt(dirToTarget.y);
+			if(_weapon != null)
+			{
+				_weapon.Aim(dirToTarget);
+				_weapon.Fire();
+			}
+		}
+	}
+
+	protected bool HasSensedSomething()
+	{
+		if(CheckForSomething)
+		{
+			int numSensed = Physics2D.OverlapCircleNonAlloc(_realSensorCenter, SensorRadius, _sensedColliders, SensorLayersToCheck.value);
+			return numSensed != 0;
+		}
+		return false;
+	}
+	
+	protected virtual void OnHit(object sender, Vector2 hitDirection)
+	{
+		if(!_stunTimer.IsRunning)
+		{
+			rigidbody2D.AddForce(HitPushForce * hitDirection, ForceMode2D.Impulse);
+			_stunTimer.Stop();
+			_stunTimer.TargetTime = HitStunTime;
+			_stunTimer.Start();
+		}
+	}
+	
+	protected virtual void OnDead(object sender, System.EventArgs e)
+	{
+		if(!_isDying)
+		{
+			_isDying = true;
+			Destroy (gameObject);
 		}
 	}
 }
--- Assets/Scripts/Weapons/BulletBehaviour.cs
+++ Assets/Scripts/Weapons/BulletBehaviour.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class BulletBehaviour : MonoBehaviour
 {
 	public int DamageValue = 0;
+	public int ImpactPoolIndex = 0;
 
 	private bool _isDying = false;
 
@@ -24,6 +25,24 @@ public class BulletBehaviour : MonoBehaviour
 			rigidbody2D.velocity = Vector2.zero;
 			collider2D.enabled = false;
 			renderer.enabled = false;
+
+			GameObject impactGO = GameManager.Instance.GetBulletImpact(ImpactPoolIndex);
+			impactGO.SetActive(true);
+			impactGO.transform.position = transform.position;
+			if(transform.rotation.y != 0f)
+			{
+				Quaternion newRot = transform.rotation;
+				newRot.y *= -1f;
+				newRot.w *= -1f;
+				impactGO.transform.rotation = newRot;
+			}
+			else
+			{
+				impactGO.transform.rotation = transform.rotation;
+			}
+
+			impactGO.renderer.enabled = true;
+
 			gameObject.SetActive(false);
 		}
 	}
